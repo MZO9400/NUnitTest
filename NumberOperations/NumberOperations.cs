b9de39bed8e21b459b93eb546a20536aeb38d8a0
MZO9400 <hamza.hameed00@gmail.com>
2021-12-23 19:14:03 +0500

    Use NUnit to test simple functions

--- NumberOperations/NumberOperations.cs
+++ NumberOperations/NumberOperations.cs
@@ -1,5 +1,35 @@
+using System;
+
 namespace NumberOperations; 
 
 public class NumberOperations {
-	
+	public int Add(int a, int b) {
+		return a + b;
+	}
+	public int Subtract(int a, int b) {
+		return a - b;
+	}
+	public int Multiply(int a, int b) {
+		return a * b;
+	}
+	public int Divide(int a, int b) {
+		return a / b;
+	}
+	public bool IsEven(int a) {
+		return a % 2 == 0;
+	}
+	public bool IsPrime(int a) {
+		if (a == 1) {
+			return false;
+		}
+		for (int i = 2; i < a; i++) {
+			if (a % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+	public int Distance(int x1, int y1, int x2, int y2) {
+		return (int)Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+	}
 }
--- NumberOperations/UnitTest1.cs
+++ NumberOperations/UnitTest1.cs
@@ -10,8 +10,39 @@ public class Tests
     }
 
     [Test]
-    public void Test1()
-    {
-        Assert.Pass();
+    public void TestAdd() {
+        Assert.AreEqual(5, new NumberOperations().Add(3, 2));
+    }
+    [Test]
+    public void TestSubtract() {
+        Assert.AreEqual(1, new NumberOperations().Subtract(3, 2));
+    }
+    [Test]
+    public void TestMultiply() {
+        Assert.AreEqual(6, new NumberOperations().Multiply(3, 2));
+    }
+    [Test]
+    public void TestDivide() {
+        Assert.AreEqual(3, new NumberOperations().Divide(9, 3));
+    }
+    [Test]
+    public void TestIsPrime() {
+        Assert.AreEqual(true, new NumberOperations().IsPrime(3));
+    }
+    [Test]
+    public void TestIsPrime2() {
+        Assert.AreEqual(false, new NumberOperations().IsPrime(4));
+    }
+    [Test]
+    public void TestIsEven() {
+        Assert.AreEqual(true, new NumberOperations().IsEven(2));
+    }
+    [Test]
+    public void TestIsEven2() {
+        Assert.AreEqual(false, new NumberOperations().IsEven(3));
+    }
+    [Test]
+    public void TestDistance() {
+        Assert.AreEqual(5, new NumberOperations().Distance(0, 0, 3, 4));
     }
 }
